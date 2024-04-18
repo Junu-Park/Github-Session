af0e712cce4b9cee58c98840414a8d6003723e9d
Arthur <ays99111@naver.com>
2024-04-18 20:15:10 +0900

    Initial Commit

---
+++ GitSession/GitSessionApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitSessionApp.swift
+//  GitSession
+//
+//  Created by Park Junwoo on 4/18/24.
+//
+
+import SwiftUI
+
+@main
+struct GitSessionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
